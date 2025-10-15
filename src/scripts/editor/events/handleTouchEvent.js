1981c79650dee6b82ceb7cf44ec2b69a52058dc1
isaac-e-butler <isaac.e.butler@gmail.com>
2025-10-15 01:36:20 +0100

    Bug Fix - Correctly Total Touches

--- src/scripts/editor/events/handleTouchEvent.js
+++ src/scripts/editor/events/handleTouchEvent.js
@@ -1,16 +1,18 @@
-export async function handleTouchEvent(editor, event) {
+export function handleTouchEvent(editor, event) {
     event.stopPropagation();
     event.preventDefault();
 
     editor.debounceCursor();
     editor.resetReceiver();
 
-    try {
-        if (event.touches !== 2) return;
+    if (event.touches.length !== 2) return;
 
-        const text = await navigator.clipboard.readText();
-        this.insertText(text);
-    } catch (error) {
-        console.error("Error occurred while pasting via touch:", error);
-    }
+    navigator.clipboard
+        .readText()
+        .then((text) => {
+            editor.insertText(text);
+        })
+        .catch((error) => {
+            console.error("Error occurred while pasting via touch:", error);
+        });
 }
